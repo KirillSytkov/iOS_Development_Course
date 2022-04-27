0dffe5a4da4a534018e1780ab45ae697b26b226c
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-04-27 19:56:41 +0300

    refactor: create generic onboarding view controller
    q

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -17,8 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         window?.backgroundColor = .white
 //        window?.rootViewController = LoginViewController()
-//        window?.rootViewController = OnboardingContainerViewController()
-        window?.rootViewController = OnboardingViewController()
+        window?.rootViewController = OnboardingContainerViewController()
         return true
     }
     
