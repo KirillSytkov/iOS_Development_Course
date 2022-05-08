5f5dd08a1d585eb382074f4dde4f49c46aff8860
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-05-08 18:56:26 +0300

    feat: shake sign button if login false

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -150,7 +150,7 @@ extension LoginViewController {
             return
         }
 
-        if username == "Kirill" && password == "Welcome" {
+        if username == "User" && password == "123456" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
         } else {
@@ -162,11 +162,26 @@ extension LoginViewController {
     private func configureView(withMessage message: String) {
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
     }
+    
+    
 }
 
 //MARK: - Animations
 extension LoginViewController {
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0,10,-10,10,0]
+        animation.keyTimes = [0,0.16,0.5,0.83,1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
+        
+    }
+    
     private func animate() {
         
         let animator1 = UIViewPropertyAnimator(duration: 1, curve: .easeInOut) {
