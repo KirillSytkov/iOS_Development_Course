bdb7a647a44cae50ccf4cbe33bfa549f3c40fcd2
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-05-14 13:06:12 +0300

    feat: create new password project

---
+++ PasswordReseter/PasswordReseter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PasswordReseter
+//
+//  Created by Kirill Sytkov on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
